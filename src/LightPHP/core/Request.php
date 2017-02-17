11fd58b67f938b8a06d8da1c9670b0e378fe2fa6
Sam Street <sam@wearenifty.co.uk>
2017-02-17 13:58:29 +0000

    request and response objects

--- src/LightPHP/core/Request.php
+++ src/LightPHP/core/Request.php
@@ -9,7 +9,44 @@
 namespace LightPHP\Core;
 
 
-class Request
-{
+use LightPHP\Interfaces\RequestInterface;
 
+class Request implements RequestInterface
+{
+    CONST M_GET     = "GET";
+    CONST M_POST    = "POST";
+    CONST M_DELETE  = "DELETE";
+
+    protected $_method = self::M_GET;
+
+    protected $_headers = [];
+
+    protected $_body = null;
+
+    function __construct($method, $headers)
+    {
+        if(!defined('static::M_' . $method))
+            throw new \Exception("Invalid method supplied");
+
+        $this->_method = $method;
+        $this->_headers = $headers;
+
+        return $this;
+
+    }
+
+    public function getHeaders()
+    {
+        // TODO: Implement getHeaders() method.
+    }
+
+    public function getQuery()
+    {
+        // TODO: Implement getQuery() method.
+    }
+
+    public function getPost()
+    {
+        // TODO: Implement getPost() method.
+    }
 }
